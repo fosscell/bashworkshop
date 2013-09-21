57c70d2217440ff6e8e329a21ca9f7041ac0b870
sudev <sudevdev@gmail.com>
2013-09-21 21:42:10 +0530

    pronunciation

--- pron.sh
+++ pron.sh
@@ -5,26 +5,29 @@
 #Creating a temp folder 
 dir=~/.dict
 
-#Check for the existence of directory, if not present create one
+#Check for the existence if not create one
 [[ -d $dir ]] || mkdir $dir
 
 
 #download respective file from dictionary dot com 
 # -q => do it quietly ie nothing @ screen 
 # -O save it as mean
-wget -q -O $dir/mean http://dictionary.reference.com/browse/$1
+wget -q -O $dir/pron http://en.wiktionary.org/wiki/$1
 
 #Please DONT hardcode the value, give it to variable and then use it 
-file=$dir/mean
+file=$dir/pron
 
 #greping out result
-m=$(cat $file | grep description | grep -Po 'content=.*.*See more' | grep -Po '\,.*.\.')
+m=$(cat $file | grep -Po '//upload.*.ogg' | grep -v type)
 
 #saving the error code 
 k=$(echo $?)
 
+wget -q -O $dir/$1.ogg http:$m 
+mplayer $dir/$1.ogg
+rm $dir/$1.ogg
 #echoing
-echo "Meaning of the word "$1" is"$m
+#echo "Meaning of the word "$1" is"$m
 
 #checks if the word was actually available else throws an error
 if [[ $k -gt 0 ]]; 
